97d89bb64ca49cbc7ac500eca6b59ae108e4594c
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-04-08 15:31:05 +0545

    ratingSelect component added

--- src/components/feedback/FeedbackForm.js
+++ src/components/feedback/FeedbackForm.js
@@ -2,28 +2,29 @@ import React from "react";
 import { useState } from "react";
 import Card from "../common/Card";
 import Button from "../common/Button";
+import RatingSelect from "./RatingSelect";
 
 const FeedbackForm = () => {
-    console.log('running');
-
   const [text, setText] = useState("");
   const [disabled, setDisabled] = useState(true);
   const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(0);
 
   function handleInputText(e) {
     setText(e.target.value);
-    if(e.target.value === '' || e.target.value.trim().length <= 5) {
-        setDisabled(true);
-        setMessage('Text should be greater than 5 character');
+    if (e.target.value === "" || e.target.value.trim().length <= 5) {
+      setDisabled(true);
+      setMessage("Text should be greater than 5 character");
     } else {
-        setDisabled(false);
-        setMessage('');
+      setDisabled(false);
+      setMessage("");
     }
   }
   return (
     <Card>
       <form>
         <h3>Please type your feedback below.</h3>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             type="text"
@@ -34,7 +35,7 @@ const FeedbackForm = () => {
             Send
           </Button>
         </div>
-        {message && <p style={{color: 'red'}}>{message}</p>}
+        {message && <p style={{ color: "red" }}>{message}</p>}
       </form>
     </Card>
   );
